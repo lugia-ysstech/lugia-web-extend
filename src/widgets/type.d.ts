82d949fa1c9277beb2d56991b7a7c848057c89f9
zenjava <138962816@qq.com>
2020-10-25 23:38:47 +0800

    修改：   * 升级devtools-widgets，解决TS打包错误。

---
+++ src/widgets/type.d.ts
@@ -0,0 +1,18 @@
+/**
+ * 忽略TS定义的模块
+ */
+declare module '@lugia/lugia-web';
+declare module '@lugia/lugia-mega-ui';
+declare module '@lugia/mega-utils';
+declare module '@lugia/mega-utils/lib/is';
+declare module '@lugia/lugia-web/dist/consts/index';
+declare module 'rc-util/lib/Dom/contains';
+declare module '*.json';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '@lugia/lugiax-router';
