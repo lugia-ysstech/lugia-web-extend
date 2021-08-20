0c93a420824f30f99e35cf53999a0f1f72a70c51
zenjava <138962816@qq.com>
2021-08-20 18:02:40 +0800

    发布：   1.1.9 删除：  * 无用代码  * 升级依赖。

--- src/widgets/button/demo/index.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import Button from '../index';
-
-export default ()=>{
-  return <React.Fragment>
-    <Button/>
-  </React.Fragment>
-}
--- src/widgets/button/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import {Button} from '@lugia/lugia-web';
-
-export default () => <Button>A button</Button>;
